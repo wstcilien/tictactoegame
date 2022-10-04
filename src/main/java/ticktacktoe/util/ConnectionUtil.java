db8697510c1cdf44e81931b667c3a6a3029a535b
wstcilien <99144980+wstcilien@users.noreply.github.com>
2022-10-04 11:56:09 -0400

    Update ConnectionUtil.java

--- src/main/java/ticktacktoe/util/ConnectionUtil.java
+++ src/main/java/ticktacktoe/util/ConnectionUtil.java
@@ -20,12 +20,12 @@ public class ConnectionUtil {
 
             Properties props = new Properties();
             props.load(propertiesInput);
-
             String url = (String) props.get("spring.datasource.url");
             System.out.println(url);
             String username = (String) props.get("spring.datasource.username");
             String password = (String) props.get("spring.datasource.password");
-
+            String driver = (String) props.get("spring.datasource.driver-class-name);
+            Class.forName(driver);
 
             if (conn == null) {
                 try {
