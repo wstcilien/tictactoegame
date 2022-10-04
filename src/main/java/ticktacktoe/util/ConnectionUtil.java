58da94e96d7e8dfb545a2dd8b25e67d02c12b437
wstcilien <99144980+wstcilien@users.noreply.github.com>
2022-10-04 12:04:04 -0400

    Update ConnectionUtil.java

--- src/main/java/ticktacktoe/util/ConnectionUtil.java
+++ src/main/java/ticktacktoe/util/ConnectionUtil.java
@@ -3,9 +3,7 @@ package ticktacktoe.util;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class ConnectionUtil {
