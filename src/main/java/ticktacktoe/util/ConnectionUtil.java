e2e6ddd6a925e2d86ab8c1dc829142868bda5f0f
wstcilien <99144980+wstcilien@users.noreply.github.com>
2022-10-04 13:00:04 -0400

    Update ConnectionUtil.java

--- src/main/java/ticktacktoe/util/ConnectionUtil.java
+++ src/main/java/ticktacktoe/util/ConnectionUtil.java
@@ -23,7 +23,7 @@ public class ConnectionUtil {
             String username = (String) props.get("spring.datasource.username");
             String password = (String) props.get("spring.datasource.password");
             String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver.class");
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
             if (conn == null) {
                 try {
